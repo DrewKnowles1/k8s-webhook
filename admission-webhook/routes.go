6b9c3e1fdd80aac59cb5f652ed753b14cd2ef85c
Andrew Knowles <andrew.knowles@control-plane.io>
2021-05-18 18:10:22 +0100

    Adding validaTION FUNCTIONALITY

--- admission-webhook/routes.go
+++ admission-webhook/routes.go
@@ -10,7 +10,7 @@ func (app *application) routes() *mux.Router {
 	router := mux.NewRouter()
 	//Get requests that go to /healtcheck will be handled by the healcheck method in the handlers.go file
 	router.HandleFunc("/healthcheck", app.healthcheck).Methods("GET")
-	router.HandleFunc("/validate", app.validate).Methods("GET")
+	router.HandleFunc("/validate", app.validate).Methods("POST")
 
 	return router
 }
